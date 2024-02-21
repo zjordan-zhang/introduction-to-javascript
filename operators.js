77896455d0930cf994dcae60b0880df9d2e9b4ec
Jordan Zhang <jordanzhang26@gmail.com>
2024-02-21 11:31:02 -0500

    feat: demonstrate usage of arithmetic and logic operators in javascript

---
+++ operators.js
@@ -0,0 +1,23 @@
+console.log('Hello World!')
+
+/* Arithmetic operators in JS:
+    + addition
+    - subtraction
+    * multiplication
+    / division
+*/
+
+/* Comparison operators in JS:
+    > greater than
+    < less than
+    == equal
+*/
+
+console.log("2 + 2 =", 2 + 2 );
+console.log("1 + 2 + 3 + 4 + 5 =", 1 + 2 + 3 + 4 + 5);
+console.log("20 - 18 =", 20 - 18);
+console.log("2 * 3 =", 2 * 3);
+console.log("8 / 1 =", 8 / 1);
+console.log("3 > 2 is", 3 > 2);
+console.log("2 > 3 is", 2 > 3);
+console.log("10 == 10 is", 10 == 10);
